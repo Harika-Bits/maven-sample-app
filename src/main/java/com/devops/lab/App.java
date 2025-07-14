d830237979d2adcdf3b548d9145c1a1c09eb123a
Harika-Bits <2021WC86237@wilp,bits-pilani.ac.in>
2025-07-14 18:19:00 +0530

    Modified App.java to test CI for Task 4

--- src/main/java/com/devops/lab/App.java
+++ src/main/java/com/devops/lab/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "riggered Jenkins CI!" );
+        System.out.println( "Jenkins CI Trigger Test!" );
     }
 }
